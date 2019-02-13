4ad24dee172db966c532e959192493a7dab8844b
Emanuele Di Santo <emdi@nordicsemi.no>
2019-02-13 14:49:34 +0100

    kernel panic

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -7,7 +7,21 @@
 #include <zephyr.h>
 #include <misc/printk.h>
 
+struct k_delayed_work dw;
+
+void work_handler(struct k_work *w)
+{
+	k_delayed_work_submit(&dw, 10);
+}
+
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+
+	k_delayed_work_init(&dw, work_handler);
+	k_delayed_work_submit(&dw, K_NO_WAIT);
+
+	while (1) {
+		k_sleep(K_SECONDS(1));
+	}
 }
