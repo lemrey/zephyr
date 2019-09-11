1d8e92474d6001291b87e67f1e6ad10f45d0011f
Emanuele Di Santo <emdi@nordicsemi.no>
2019-09-11 16:03:50 +0200

    samples: hello_world: demonstrate k_delayed_work behavior
    No error is returned, and timer expires immediately.
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -7,7 +7,24 @@
 #include <zephyr.h>
 #include <sys/printk.h>
 
+static struct k_delayed_work dwork;
+
+void work_item(struct k_work *w)
+{
+	printk("That wasn't 20 hours..\n");
+	/* k_delayed_work_submit(&dwork, K_HOURS(20)); */
+}
+
 void main(void)
 {
+	int err;
+
 	printk("Hello World! %s\n", CONFIG_BOARD);
+
+	k_delayed_work_init(&dwork, work_item);
+
+	err = k_delayed_work_submit(&dwork, K_HOURS(20));
+	if (err) {
+		printk("k_delayed_work_submit(), err %d", err);
+	}
 }
